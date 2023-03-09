f3bdcf3d70286db7c71d60b1c02c99293f613f00
GhayathQuraan <ghayath04@hotmail.com>
2023-03-09 22:09:28 +0300

    ghayath

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeHeaderComponent,
+    HomeFooterComponent
+  ],
   imports: [
     CommonModule
   ]
