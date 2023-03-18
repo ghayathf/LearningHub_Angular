484fccb7645cd433b8eb5d90de0d5a488026ea39
Ahmadghazal1 <74017377+Ahmadghazal1@users.noreply.github.com>
2023-03-18 04:07:42 +0300

    xx

--- src/app/admin/trainee-requests/trainee-requests.component.ts
+++ src/app/admin/trainee-requests/trainee-requests.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { RegisterService } from 'src/app/register.service';
 
 @Component({
@@ -7,9 +8,24 @@ import { RegisterService } from 'src/app/register.service';
   styleUrls: ['./trainee-requests.component.css']
 })
 export class TraineeRequestsComponent {
-  constructor(public TraineeService: RegisterService) { }
+  @ViewChild('DeleteForm') Delete: any
+  constructor(public TraineeService: RegisterService, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.TraineeService.GetAllTraineeUser();
   }
+  selectedItem = 0
+  openDeleteDialog(id: number) {
+    this.selectedItem = id;
+    this.dialog.open(this.Delete);
+  }
+  // async DeleteRequest() {
+  //   await this.TraineeService.DeleteRequest(this.selectedItem);
+  //   this.TraineeService.GetAllTraineeUser();
+  // }
+
+  // ChangeStatus(request: any) {
+  //   this.TraineeService.UpdateRequest(request);
+  //   this.TraineeService.GetAllTraineeUser();
+  // }
 }
